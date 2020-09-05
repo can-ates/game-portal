9392b347717bcece42593725b5c172a0897aa842
CYMAD <40774917+CYMAD@users.noreply.github.com>
2020-09-05 04:48:19 +0300

    upload image route added

--- firebase-functions/functions/handlers/users.js
+++ firebase-functions/functions/handlers/users.js
@@ -2,7 +2,7 @@ const { admin, db } = require('../util/admin');
 
 const config = require('../util/config');
 const { validationResult } = require('express-validator');
-
+const { v4: uuid } = require('uuid');
 
 const firebase = require('firebase');
 firebase.initializeApp(config);
@@ -35,12 +35,10 @@ exports.signup = (req, res) => {
           .auth()
           .createUserWithEmailAndPassword(signUpUser.email, signUpUser.password)
           .then(userData => {
-            
             userId = userData.user.uid;
             return userData.user.getIdToken();
           })
           .then(idToken => {
-            
             token = idToken;
             const credentials = {
               userId,
@@ -58,7 +56,6 @@ exports.signup = (req, res) => {
       }
     })
     .catch(err => {
-      
       if (err.code === 'auth/email-already-in-use') {
         return res.status(400).json({ email: 'Email is already in use' });
       } else {
@@ -107,12 +104,75 @@ exports.login = (req, res) => {
 };
 
 exports.uploadImage = (req, res) => {
-    const Busboy = require('busboy')
+  const Busboy = require('busboy');
+  const path = require('path');
+  const os = require('os');
+  const fs = require('fs');
 
-    const busboy = new Busboy({headers: req.headers})
+  let imageToBeUploaded = {};
+  let imageFileName;
+  // String for image token
+  let generatedToken = uuid();
+
+  const busboy = new Busboy({ headers: req.headers });
+
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    
+    if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+      return res.status(400).json({ error: 'Wrong file type submitted' });
+    }
+    file.on('data', data => {
+      if (data.length > 125000) {
+        return res
+          .status(400)
+          .json({ error: 'Image can not be larger than 1MB' });
+      }
+    });
+
+    
+    const imageExtension = filename.split('.')[
+      filename.split('.').length - 1
+    ];
+    // 32756238461724837.png
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
+    //filedan alinan veriyi okuyup filepathe yaziyoruz/// Temp klasoru
+    file.pipe(fs.createWriteStream(filepath));
+    
+  });
+  busboy.on("finish", () => {
+    
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype,
+            //Generate token to be appended to imageUrl
+            firebaseStorageDownloadTokens: generatedToken,
+          },
+        },
+      })
+      .then(() => {
+        // Append token to url
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media&token=${generatedToken}`;
+        return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: "image uploaded successfully" });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: "something went wrong" });
+      });
+  });
   
-    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-    })
 
-    busboy.end(req.rawBody)
-}
+  busboy.end(req.rawBody);
+};
