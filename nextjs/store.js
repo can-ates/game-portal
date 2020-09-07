c61a92ca7a96168d7bb7254a461efcfd2e44e0b2
CYMAD <40774917+CYMAD@users.noreply.github.com>
2020-09-07 04:39:27 +0300

    redux reinit with thunk

--- nextjs/store.js
+++ nextjs/store.js
@@ -1,48 +1,16 @@
 import { useMemo } from 'react'
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunkMiddleware from 'redux-thunk'
+import reducers from './reducers'
 
 let store
 
-const initialState = {
-  lastUpdate: 0,
-  light: false,
-  count: 3,
-}
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'TICK':
-      return {
-        ...state,
-        lastUpdate: action.lastUpdate,
-        light: !!action.light,
-      }
-    case 'INCREMENT':
-      return {
-        ...state,
-        count: state.count + 1,
-      }
-    case 'DECREMENT':
-      return {
-        ...state,
-        count: state.count - 1,
-      }
-    case 'RESET':
-      return {
-        ...state,
-        count: initialState.count,
-      }
-    default:
-      return state
-  }
-}
-
-function initStore(preloadedState = initialState) {
+function initStore(initialState) {
   return createStore(
-    reducer,
-    preloadedState,
-    composeWithDevTools(applyMiddleware())
+    reducers,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
   )
 }
 
